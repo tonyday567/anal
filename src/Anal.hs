6134c4ed6d3278a5defbba97fbc8ee28f1bcbd7a
Tony Day <tonyday567@gmail.com>
2023-04-18 08:08:35 +1000

    nh11 changes

--- src/Anal.hs
+++ src/Anal.hs
@@ -1,48 +1,48 @@
-{-# OPTIONS_GHC -Wall #-}
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE BangPatterns #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-{-# HLINT ignore "Use <$>" #-}
-{-# LANGUAGE RebindableSyntax #-}
 {-# LANGUAGE MonoLocalBinds #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# HLINT ignore "Use <$>" #-}
+{-# LANGUAGE RebindableSyntax #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# OPTIONS_GHC -Wall #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# OPTIONS_GHC -Wno-unused-imports #-}
+{-# OPTIONS_GHC -Wno-unused-top-binds #-}
 
-module Anal
-where
+module Anal where
 
-import qualified Prelude
-import FlatParse.Basic
-    ( Parser,
-      some,
-      empty,
-      Result(OK, Fail, Err),
-      satisfyAscii,
-      isDigit,
-      chainr,
-      withOption,
-      char,
-      (<|>),
-      runParser,
-      satisfy,
-      strToUtf8 )
-import Data.Time.Calendar
-import Data.Char hiding (isDigit)
-import qualified Data.ByteString as BS
-import Data.Mealy
 import Control.Category (id)
-import Data.Maybe
-import Data.Profunctor
-import qualified Data.ByteString.Char8 as C
-import qualified Data.Map as Map
 import Data.Bifunctor
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as C
+import Data.Char hiding (isDigit)
 import Data.Foldable
-import NumHask.Prelude hiding (id)
-import Data.Text ( Text, unpack )
 import Data.FormatN
-import Data.Time.Format.ISO8601
+import qualified Data.Map as Map
+import Data.Maybe
+import Data.Mealy
 import Data.Mealy.Quantiles
+import Data.Profunctor
+import Data.Text (Text, unpack)
+import Data.Time.Calendar
+import Data.Time.Format.ISO8601
+import FlatParse.Basic
+  ( Parser,
+    Result (Err, Fail, OK),
+    chainr,
+    char,
+    empty,
+    isDigit,
+    runParser,
+    satisfy,
+    satisfyAscii,
+    some,
+    strToUtf8,
+    withOption,
+    (<|>),
+  )
+import NumHask.Prelude hiding (id)
+import qualified Prelude
 
 -- $setup
 --
@@ -54,7 +54,8 @@ import Data.Mealy.Quantiles
 
 -- | Parse a digit
 digit :: Parser a Int
-digit = (\c -> ord c - ord '0') <$> satisfyAscii isDigit
+digit =
+  (\c -> ord c - ord '0') <$> satisfyAscii isDigit
 
 -- | (unsigned) Int parser
 --
@@ -105,7 +106,8 @@ double = do
 -- >>> runParser (signed double) "-1.234x"
 -- OK (-1.234) "x"
 signed :: (Ring b, FromInteger b) => Parser e b -> Parser e b
-signed p = withOption $(char '-') (const (((-1) *) <$> p)) p
+signed p =
+  withOption $(char '-') (const (((-1) *) <$> p)) p
 
 -- | Day parser, consumes separator
 --
@@ -147,33 +149,32 @@ quoted :: Parser e a -> Parser e a
 quoted p = $(char '"') *> p <* $(char '"')
 
 numString :: Parser e String
-numString = filter (/= ',') <$> some (satisfy (\x -> isDigit x || (x=='.') || (x==',')))
+numString = filter (/= ',') <$> some (satisfy (\x -> isDigit x || (x == '.') || (x == ',')))
 
 -- | Not great because will always succeed
 num :: Parser e (Maybe Double)
-num = runParserMaybe double . strToUtf8 . filter (/=',') <$> numString
+num = runParserMaybe double . strToUtf8 . filter (/= ',') <$> numString
 
 auinvP :: Parser e (Day, String)
 auinvP = (,) <$> quoted dayP' <*> (($(char ',')) *> quoted numString)
 
-
 -- return scan
 
 ret :: Mealy (Day, Double) (Day, Double)
-ret = second' ((\p p' -> log (p/p')) <$> id <*> delay1 undefined)
+ret = second' ((\p p' -> log (p / p')) <$> id <*> delay1 undefined)
 
-getPricesFred :: IO [(Day,Double)]
+getPricesFred :: IO [(Day, Double)]
 getPricesFred = do
   bs <- BS.readFile "other/SP500.csv"
-  pure $ [(d,p) | Just (d, Right p) <- runParserMaybe fredP <$> drop 1 (C.lines bs)]
+  pure $ [(d, p) | Just (d, Right p) <- runParserMaybe fredP <$> drop 1 (C.lines bs)]
 
-getPricesAuinv :: FilePath -> IO [(Day,Double)]
+getPricesAuinv :: FilePath -> IO [(Day, Double)]
 getPricesAuinv fp = do
   bs <- BS.readFile fp
-  let pricesString = [(d,p) | Just (d, p) <- runParserMaybe auinvP <$> drop 1 (C.lines bs)]
-  pure [(d,p) | (d, Just p) <- second (runParserMaybe double . strToUtf8) <$> pricesString]
+  let pricesString = [(d, p) | Just (d, p) <- runParserMaybe auinvP <$> drop 1 (C.lines bs)]
+  pure [(d, p) | (d, Just p) <- second (runParserMaybe double . strToUtf8) <$> pricesString]
 
-getPricesAuinvs :: IO [(Day,Double)]
+getPricesAuinvs :: IO [(Day, Double)]
 getPricesAuinvs = do
   p1 <- getPricesAuinv "other/S&P 500 Historical Data.csv"
   p4 <- getPricesAuinv "other/S&P 500 Historical Data (4).csv"
@@ -181,7 +182,7 @@ getPricesAuinvs = do
   let pricesMap = Map.unions [Map.fromList p1, Map.fromList p4, Map.fromList p5]
   pure $ Map.toList pricesMap
 
-getPrices :: IO [(Day,Double)]
+getPrices :: IO [(Day, Double)]
 getPrices = do
   pFred <- getPricesFred
   pAuinv <- getPricesAuinvs
@@ -200,7 +201,7 @@ taker n = reverse . take n . reverse
 
 signalize :: Double -> [Double] -> Mealy Double Double
 signalize r qs' =
-  (\x -> fromIntegral x/fromIntegral (length qs' + 1)) <$> digitize r qs'
+  (\x -> fromIntegral x / fromIntegral (length qs' + 1)) <$> digitize r qs'
 
 defaultQuantiles :: [Double]
 defaultQuantiles = (0.1 *) <$> [1 .. 9]
@@ -208,7 +209,7 @@ defaultQuantiles = (0.1 *) <$> [1 .. 9]
 quantileNames :: [Double] -> [Text]
 quantileNames qs = (<> "th") . comma (Just 0) . (100 *) <$> qs
 
-getOriginalReturns :: IO [(Day,Double)]
+getOriginalReturns :: IO [(Day, Double)]
 getOriginalReturns = do
   p <- getPrices
   pure $ drop 1 $ scan ret p
@@ -220,11 +221,11 @@ makeReturns = do
 
 writeReturns :: [(Day, Double)] -> IO ()
 writeReturns r =
-  writeFile "other/returns.csv" .
-  unlines $
-  (\(d,r') -> d <> "," <> unpack r') .
-  bimap (formatShow iso8601Format) (fixed (Just 6)) <$>
-  r
+  writeFile "other/returns.csv"
+    . unlines
+    $ (\(d, r') -> d <> "," <> unpack r')
+      . bimap (formatShow iso8601Format) (fixed (Just 6))
+      <$> r
 
 runParserError :: Parser e a -> BS.ByteString -> a
 runParserError p bs = case runParser p bs of
@@ -234,7 +235,7 @@ runParserError p bs = case runParser p bs of
 dayReturnP :: Parser e (Day, Double)
 dayReturnP = (,) <$> dayP <*> ($(char ',') *> signed double)
 
-getReturns :: IO [(Day,Double)]
+getReturns :: IO [(Day, Double)]
 getReturns = do
   bs <- BS.readFile "other/returns.csv"
   pure $ runParserError dayReturnP <$> C.lines bs
